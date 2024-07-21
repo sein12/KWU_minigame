ce9041f57b281bbad6cce7538b85058267dd1e8e
KimSesame <kimbs082787@gmail.com>
2024-07-21 18:49:37 +0900

    feat: 수박게임 다양한 원 생성
    생성되는 원형 물체의 종류를 다양화한다.

---
+++ Assets/Scripts/Watermelon/GameManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GradeBall lastGradeBall;
+    public GameObject gradeBallPrefab;
+    public Transform gradeBallGroup;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    void Start()
+    {
+        NextGradeBall();
+    }
+
+    GradeBall GetGradeBall()
+    {
+        GameObject instant = Instantiate(gradeBallPrefab, gradeBallGroup);
+        GradeBall instantGradeBall = instant.GetComponent<GradeBall>();
+        return instantGradeBall;
+    }
+
+    void NextGradeBall()
+    {
+        GradeBall newGradeBall = GetGradeBall();
+        lastGradeBall = newGradeBall;
+        lastGradeBall.level = Random.Range(0, 3);   // F, D, C
+        lastGradeBall.gameObject.SetActive(true);
+
+        StartCoroutine("WaitNextGradeBall");
+    }
+
+    IEnumerator WaitNextGradeBall()
+    {
+        while (lastGradeBall != null)
+            yield return null;
+
+        yield return new WaitForSeconds(2.5f);
+
+        NextGradeBall();
+    }
+
+    public void TouchDown()
+    {
+        if (lastGradeBall == null) return;
+
+        lastGradeBall.Drag();
+    }
+
+    public void TouchUp()
+    {
+        if (lastGradeBall == null) return;
+
+        lastGradeBall.Drop();
+        lastGradeBall = null;
+    }
+}
--- Assets/Scripts/Watermelon/GradeBall.cs
+++ Assets/Scripts/Watermelon/GradeBall.cs
@@ -1,15 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
-public class Circle : MonoBehaviour
+public class GradeBall : MonoBehaviour
 {
+    public enum GradeLevel
+    {
+        F, D, C, B, A, Aplus, S
+    }
+
     public bool isDrag;
+    public int level;
     Rigidbody2D rb;
+    Animator anim;
+    TextMeshPro textMeshPro;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        textMeshPro = GetComponentInChildren<TextMeshPro>();
+    }
+
+    // OnEnable is called script is enabled
+    void OnEnable()
+    {
+        anim.SetInteger("Level", level);
+
+        string text;
+        switch ((GradeLevel)level)
+        {
+            case GradeLevel.F:
+                text = "F";
+                break;
+            case GradeLevel.D:
+                text = "D";
+                break;
+            case GradeLevel.C:
+                text = "C";
+                break;
+            case GradeLevel.B:
+                text = "B";
+                break;
+            case GradeLevel.A:
+                text = "A";
+                break;
+            case GradeLevel.Aplus:
+                text = "A+";
+                break;
+            case GradeLevel.S:
+                text = "S";
+                break;
+            default:
+                text = "Error";
+                break;
+        }
+        textMeshPro.text = text;
     }
 
     // Update is called once per frame
