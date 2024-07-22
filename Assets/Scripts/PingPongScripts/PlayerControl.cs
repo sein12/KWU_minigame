92222be6be3184b02d8380f141429d0e7e9cc6ad
enchupin <dpscbvls8358@naver.com>
2024-07-22 21:55:31 +0900

    캐릭터 이동 구현
    우선 키보드 WASD로 이동 구현, 조이스틱으로 움직임은 추후 구현 필요

--- Assets/Scripts/PingPongScripts/PlayerControl.cs
+++ Assets/Scripts/PingPongScripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
@@ -8,10 +9,12 @@ using TMPro;
 
 public class PlayerControl : MonoBehaviourPunCallbacks, IPunObservable
 {
+    public Vector2 InputVec;
     public Rigidbody2D RB;
     public PhotonView PV;
     public Collider2D CD;
     public TextMeshProUGUI NickNameText;
+    public Transform UiJoyStick;
 
     void Awake()
     {
@@ -26,13 +29,26 @@ public class PlayerControl : MonoBehaviourPunCallbacks, IPunObservable
         
     }
 
-    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        Vector2 nextVec = InputVec * Time.fixedDeltaTime * 3;
+        RB.MovePosition(RB.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+            InputVec = value.Get<Vector2>();
+    }
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
+    
+
     void Update()
     {
         
